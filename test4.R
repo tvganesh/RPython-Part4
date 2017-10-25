f7df53617d079a6892bb3b5eb87ff820decbcce4
Tinniam V Ganesh <tvganesh.85@gmail.com>
2017-10-25 19:27:24 +0530

    Changes

--- test4.R
+++ test4.R
@@ -145,3 +145,22 @@ seqLogSpace <- function(start,stop,len){
 rpart = NULL
 m <-rpart(Species~.,data=iris)
 rpart.plot(m,extra=2,main="Decision Tree - IRIS")
+
+
+#https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+# load the dataset
+data(PimaIndiansDiabetes)
+# prepare training scheme
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(diabetes~., data=cancer, method="lvq", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
